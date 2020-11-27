8cc920f4dc6872349d0068b991f354ef0499a268
vietmazze <dqnguyen2011@gmail.com>
2020-11-26 20:42:55 -0500

    fixed up commands

--- binance_bulkOrder.py
+++ binance_bulkOrder.py
@@ -41,7 +41,8 @@ class BinanceClient:
                             f'reduceOnly:{result["reduceOnly"]} '))
 
         except Exception as e:
-            self.log.red(f'Failed to create order for order: {kwargs}')
+            self.log.red(
+                f'Failed to create order for order: {kwargs} \n message: {e}')
 
     ############################
         # -PLACE CONDITIONAL ORDER
@@ -58,7 +59,8 @@ class BinanceClient:
                             f'reduceOnly:{result["reduceOnly"]}'))
 
         except Exception as e:
-            self.log.red(f'Failed to create order for order: {kwargs} -- {e}')
+            self.log.red(
+                f'Failed to create order for order: {kwargs} \n message:{e}')
         ############################
         # -CANCEL ORDERS
         ############################
@@ -70,7 +72,7 @@ class BinanceClient:
             self.log.green(f'CANCEL ALL ORDER: {result["msg"]}')
         except Exception as e:
             self.log.red(
-                f'Unable to delete all orders in cancel_all_orders() -- {e}')
+                f'Unable to delete all orders in cancel_all_orders() \n message: {e}')
 
     def cancel_order(self, orderId) -> None:
         data = {"symbol": self.market,
@@ -83,7 +85,7 @@ class BinanceClient:
                             f'qty: {result["origQty"]}'
                             ))
         except Exception as e:
-            self.log.red(f'Failed to cancel_order {orderId} -- {e}')
+            self.log.red(f'Failed to cancel_order {orderId} \n message: {e}')
 
         ############################
         # -GET OPEN ORDERS
@@ -147,7 +149,124 @@ class BinanceClient:
         ##############################
         # -ORDER CLEANUP
         ###############################
+    def place_order_cleanup(self,currCommand):
+        try:
+            side = currCommand[0].upper() if len(currCommand) > 0 else None
+            self.orderSide = side
+            size = float(currCommand[1]) if len(currCommand) > 1 else 0
+            if len(currCommand) > 2:
+                    if "@" in currCommand[2]:
+                        price = currCommand[2].replace('@', '')
+                    else:
+                        price = currCommand[2]
+            else:
+                price = None
+            type = "LIMIT" if len(currCommand) > 2 else "MARKET"
+            
+            if type == "LIMIT":
+                data = {
+                    "symbol": self.market,
+                    "side": side,
+                    "type": type,
+                    "quantity": float(size) if size else None,
+                    "price": float(price) if price else None,
+                    "timeInForce": "GTC"
+                }
+            else:
+                data =  {
+                    "symbol": self.market,
+                    "side": side,
+                    "type": type,
+                    "quantity": float(size) if size else None,
+                }
+               
+                
+            self.log.green(data)
+            if size:
+                if float(size)<float(self.fatFinger):
+                    self.create_order(**data)
+        except Exception as e:
+            self.log.red(f'Error in place_order_cleanup() , please check entry: {e}')
+        
 
+    def place_conditional_order_cleanup(self,currCommand):
+
+        try:
+            if len(currCommand) > 4:
+                if "@" in currCommand[4]:
+                    limitPrice = currCommand[4].replace('@', '')
+                else:
+                    limitPrice = currCommand[4]
+            else:
+                limitPrice = None
+
+            type = currCommand[0] if len(currCommand) > 0 else None
+            if type == "tp":
+                if limitPrice:
+                    type="TAKE_PROFIT"
+                else:
+                    type = "TAKE_PROFIT_MARKET"
+            if type == "trail":
+                type = "TRAILING_STOP_MARKET"
+            if type == "stop":
+                if limitPrice:
+                    type="STOP"
+                else:
+                    type = "STOP_MARKET"
+
+            size = currCommand[1] if len(currCommand) > 1 else None
+            if len(currCommand) > 2:
+                if "@" in currCommand[2]:
+                    price = currCommand[2].replace('@', '')
+                else:
+                    price = currCommand[2]
+            else:
+                price = None
+
+            try:
+
+                if self.orderSide and not limitPrice:
+                    side = "BUY" if self.orderSide == "SELL" else "SELL"
+                elif len(currCommand) > 3:
+                    side = currCommand[3].upper()
+                else:
+                    self.log.red(
+                        f'Error in placing conditional order,need to assign a side order')
+            except Exception as e:
+                self.log.red(
+                    'cleanup conditional orderSide not assign correctly')
+            data = {}
+            if limitPrice:
+                data = {
+                    "symbol":self.market,
+                    "side": side,
+                    "type":type,
+                    "quantity": float(size) if size else None,
+                    "stopPrice": float(price) if price else None,
+                    "price": float(limitPrice) if limitPrice else None,
+                    "reduceOnly": True
+                }
+                
+            else:
+               data = {
+                "symbol":self.market,
+                    "side": side,
+                    "type":type,
+                    "quantity": float(size) if size else None,
+                    "stopPrice": float(price) if price else None,
+                    "reduceOnly": True
+                }
+            self.log.green(data)
+            if size and float(size) < float(self.fatFinger):
+                if price:
+                    self.create_conditional_order(**data)
+                else:
+                    self.log.red(f'Error in placing conditional order cleanup, missing stopPrice or limit_price')
+            else:
+                self.cp.red(
+                    f'Error in placing conditional order cleanup,need size order or exceeds fatFinger: {self.fatFinger}')
+        except Exception as e:
+            self.cp.red(f'Error in place_conditional_order_cleanup: {e}')
 
 def _get_time_offset(self):
     res = self.b.get_server_time()
